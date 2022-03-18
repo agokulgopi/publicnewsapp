b99cfc6e603806544321e3096217076f3a693843
Akhil Balakrishnan <akhilbkrishnan2000@gmail.com>
2022-03-18 16:30:04 +0530

    Navigation drawer added.

--- app/src/main/java/com/example/dailynews/LoginActivity.java
+++ app/src/main/java/com/example/dailynews/LoginActivity.java
@@ -5,17 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     TextView register;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginBtn = findViewById(R.id.loginBtn);
         register = findViewById(R.id.register);
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, NavigationDrawerActivity.class);
+                finish();
+                startActivity(i);
+            }
+        });
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
